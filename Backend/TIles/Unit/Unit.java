a8c2da61013c9adb41bc4c49c090472a315d1470
kayatsa <kayatsa8@gmail.com>
2024-11-04 19:44:21 +0200

    Backend: Tiles: Unit: adding attributes to Unit and inhereted methods.

--- Backend/TIles/Unit/Unit.java
+++ Backend/TIles/Unit/Unit.java
@@ -1,4 +1,52 @@
 package Backend.Tiles.Unit;
 
-public class Unit {
+import Backend.Tiles.EmptyTile;
+import Backend.Tiles.Tile;
+import Backend.Tiles.Wall;
+import Backend.Tiles.Unit.Enemy.Enemy;
+import Backend.Tiles.Unit.Player.Player;
+import Backend.VisitorInterfaces.Visitor;
+
+public abstract class Unit extends Tile implements Visitor{
+    protected String name;
+    protected int healthPool;
+    protected int currentHealth;
+    protected int attackPoints;
+    protected int defensePoints;
+
+    
+    
+
+
+
+
+    @Override
+    public void accept(Visitor visitor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+    }
+
+    @Override
+    public void visit(Wall wall) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+    }
+
+    @Override
+    public void visit(EmptyTile emptyTile) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+    }
+
+    @Override
+    public void visit(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+    }
+
+    @Override
+    public void visit(Enemy enemy) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+    }
 }
