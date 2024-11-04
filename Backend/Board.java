7271115b6a25b78f22f2c559702163fccf5c6753
kayatsa <kayatsa8@gmail.com>
2024-11-04 19:23:12 +0200

    Backend: Board: added getTile and updatePositions declarations for future use in units.

--- Backend/Board.java
+++ Backend/Board.java
@@ -1,4 +1,21 @@
 package Backend;
 
+import java.util.List;
+
+import javax.management.RuntimeErrorException;
+
+import Backend.Tiles.Tile;
+
 public class Board {
+
+
+
+    public Tile getTile(int x, int y){
+        throw new RuntimeException("not implemented yet");
+    }
+
+    public void updatePositions(List<Tile> tiles){
+        throw new RuntimeException("not implemented yet");
+    }
+
 }
