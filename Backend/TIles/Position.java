0af557452cec0fb32b1f0629c275d316c1098846
kayatsa <kayatsa8@gmail.com>
2024-11-04 18:44:01 +0200

    Backend: Tiles: Position: added constructor and static range method.

--- Backend/TIles/Position.java
+++ Backend/TIles/Position.java
@@ -1,4 +1,18 @@
 package Backend.TIles;
 
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double range(Position p1, Position p2){
+        throw new RuntimeException("not implemented yet");
+    }
+
+
+
 }
