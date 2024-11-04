2dfaa666dc6b953593e2def5c6245bd2341752b7
kayatsa <kayatsa8@gmail.com>
2024-11-04 21:35:05 +0200

    Backend: Tiles: EmptyTile: removing changes to avoid conflicts. TODO: merge with EmptyTile branch before merging to main.

--- Backend/TIles/EmptyTile.java
+++ Backend/TIles/EmptyTile.java
@@ -1,12 +1,4 @@
 package Backend.Tiles;
 
-import Backend.VisitorInterfaces.Visitor;
-
-public class EmptyTile extends Tile {
-
-    @Override
-    public void accept(Visitor visitor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
-    }
+public class EmptyTile {
 }
