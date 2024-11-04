83477bdd543b5d5bb1093918ceceecb91bde4219
kayatsa <kayatsa8@gmail.com>
2024-11-04 19:01:09 +0200

    Backend: Tiles: Tile: implements Visited; added range and toString methods.

--- Backend/TIles/Tile.java
+++ Backend/TIles/Tile.java
@@ -1,4 +1,18 @@
 package Backend.Tiles;
 
-public class Tile {
+import Backend.VisitorInterfaces.Visited;
+
+public abstract class Tile implements Visited{
+    protected char tile;
+    protected Position position;
+
+
+    public double range(Tile other){
+        return Position.range(position, other.position);
+    }
+
+    public String toString(){
+        return "" + tile;
+    }
+
 }
