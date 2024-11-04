1049645b08d6c3574f672f76c5064e67f68df165
kayatsa <kayatsa8@gmail.com>
2024-11-04 18:51:42 +0200

    Backend: Tiles: Position: added getters, setters, toString methods.

--- Backend/TIles/Position.java
+++ Backend/TIles/Position.java
@@ -9,6 +9,30 @@ public class Position {
         this.y = y;
     }
 
+    public int getX(){
+        return x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public String toString(){
+        return "<" + x + ", " + y + ">";
+    } 
+
+
+
+
+
     public static double range(Position p1, Position p2){
         throw new RuntimeException("not implemented yet");
     }
