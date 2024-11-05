a1fad1b2332bf5f11247493c4e3285ffbd67f1f5
kayatsa <kayatsa8@gmail.com>
2024-11-05 19:31:05 +0200

    HeroicUnit: replaced castAbility(List<Unit>) with castAbility(List<Enemy>) and castAbility(Player)

--- Backend/TIles/Unit/HeroicUnit.java
+++ Backend/TIles/Unit/HeroicUnit.java
@@ -2,9 +2,14 @@ package Backend.Tiles.Unit;
 
 import java.util.List;
 
+import Backend.Tiles.Unit.Enemy.Enemy;
+import Backend.Tiles.Unit.Player.Player;
+
 public interface HeroicUnit {
 
-    void castAbility(List<Unit> units);
+    void castAbility(List<Enemy> enemies);
+
+    void castAbility(Player player);
 
 
 }
