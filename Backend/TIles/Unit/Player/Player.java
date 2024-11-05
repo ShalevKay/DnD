cd6f4a80e0b2c6b4adbf6eac915972e6c6eccb28
kayatsa <kayatsa8@gmail.com>
2024-11-05 16:18:33 +0200

    temp

--- Backend/TIles/Unit/Player/Player.java
+++ Backend/TIles/Unit/Player/Player.java
@@ -1,13 +1,30 @@
 package Backend.Tiles.Unit.Player;
 
 import Backend.Tiles.Unit.HeroicUnit;
+import Backend.Tiles.Unit.Unit;
 
-public abstract class Player implements HeroicUnit{
+public abstract class Player extends Unit implements HeroicUnit{
     protected int experience;
     protected int level;
 
 
-    
+    protected boolean canLevelUp(){
+        return experience >= level * 50;
+    }
+
+    protected void levelUp() throws Exception{
+        if(!canLevelUp()){
+            throw new Exception("cannot level up");
+        }
+
+        experience -= 50 * level;
+        level++;
+        healthPool += 10 * level;
+        currentHealth = healthPool;
+        attackPoints += 4 * level;
+        defensePoints += level;
+    }
+
 
 
 
