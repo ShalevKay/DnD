e42a092c9a341dd2943fb4e573ca255326962eaa
kayatsa <kayatsa8@gmail.com>
2024-11-14 22:30:57 +0200

    Backend.Tiles.Units.Player.Rogue: added class, constructor, levelUp, onTick.

---
+++ Backend/Tiles/Unit/Player/Rogue.java
@@ -0,0 +1,56 @@
+package Backend.Tiles.Unit.Player;
+
+import java.util.List;
+
+import Backend.Board;
+import Backend.Tiles.Unit.Enemy.Enemy;
+
+public class Rogue extends Player {
+    private final static int ENERGY = 100;
+    
+    private int currentEnergy;
+    private int cost;
+
+    
+    public Rogue(char tile, int x, int y, String name, int healthPool, int attackPoints, int defensePoints, int cost){
+        super(tile, x, y, name, healthPool, attackPoints, defensePoints);
+
+        this.currentEnergy = ENERGY;
+        this.cost = cost;
+    }
+
+    @Override
+    protected void levelUp() throws Exception{
+        super.levelUp();
+
+        currentEnergy = ENERGY;
+        attackPoints += 3 * level;
+    }
+
+    @Override
+    public void onTick(char action, Board board, List<Enemy> enemies) throws Exception{
+        super.onTick(action, board, enemies);
+
+        currentEnergy += 10;
+
+        if(currentEnergy > ENERGY){
+            currentEnergy = ENERGY;
+        }
+
+    }
+
+
+
+    @Override
+    public void castAbility(List<Enemy> enemies) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'castAbility'");
+    }
+
+    @Override
+    public void castAbility(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'castAbility'");
+    }
+    
+}
