40e05856e2d0f292c0bb2c216e37bdfcb845ef70
kayatsa <kayatsa8@gmail.com>
2024-11-15 16:07:02 +0200

    Backend/Tiles/Unit/Enemy/Monster: fields + constructor + onTick skelleton.

---
+++ Backend/Tiles/Unit/Enemy/Monster.java
@@ -0,0 +1,49 @@
+package Backend.Tiles.Unit.Enemy;
+
+import Backend.Tiles.Unit.Player.Player;
+
+public class Monster extends Enemy{
+    private final int visionRange;
+
+
+    public Monster(char tile, int x, int y, String name, int healthPool,
+                    int attackPoints, int defensePoints, int experienceValue, int visionRange){
+        super(tile, x, y, name, healthPool, attackPoints, defensePoints, experienceValue);
+
+        this.visionRange = visionRange;
+    }
+
+
+    @Override
+    public void onTick(Player player) {
+        double range = range(player);
+        int dx, dy;
+
+        if(range < visionRange){
+            dx = position.getX() - player.getPosition().getX();
+            dy = position.getY() - player.getPosition().getY();
+
+            if(Math.abs(dx) > Math.abs(dy)){
+                if(dx > 0){
+                    // move left
+                }
+                else{
+                    // move right
+                }
+            }
+            else{
+                if(dy > 0){
+                    // move up
+                }
+                else{
+                    // move down
+                }
+            }
+        }
+        else{
+            // random movement
+        }
+    }
+
+    
+}
