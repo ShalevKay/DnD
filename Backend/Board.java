59f10a5bdc50997e8be0a71b391b339d29a8c3ea
kayatsa <kayatsa8@gmail.com>
2024-11-18 17:13:58 +0200

    Backend/Board: board field, constructor, loadLevel + read level file, getTile. TODO: implement charToTile after factory. maybe give board factory fields.

--- Backend/Board.java
+++ Backend/Board.java
@@ -1,14 +1,64 @@
 package Backend;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import Backend.Tiles.Tile;
 import Backend.Tiles.Unit.Unit;
 
 public class Board {
+    private Tile[][] board;
 
 
+    public Board(){
+        board = null;
+    }
 
+    public void loadLevel(int level) throws FileNotFoundException{
+        List<String> list = file2List("levels/level" + level + ".txt");
+        String line;
+
+        board = new Tile[list.size()][list.get(0).length()];
+
+        for(int i=0; i<list.size(); i++){
+            line = list.get(i);
+
+            for(int j=0; j<line.length(); j++){
+                board[i][j] = charToTile(line.charAt(j));
+            }
+        }
+    }
+
+    private List<String> file2List(String path) throws FileNotFoundException{
+        List<String> list = new ArrayList<>();
+
+        File file = new File(path);
+            Scanner reader = new Scanner(file);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                list.add(line);
+            }
+
+            reader.close();
+
+        return list;
+    }
+
+    private Tile charToTile(char c){
+        // TODO: use factory
+        return null;
+    }
+    
     public Tile getTile(int x, int y){
-        throw new RuntimeException("not implemented yet");
+        if(x < 0 || x > board[0].length || y < 0 || y > board.length){
+            return null;
+        }
+
+        return board[y][x];
     }
 
     public void updatePositions(Unit u1, Tile tile){
