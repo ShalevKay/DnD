9aa1595a590fcbb4c98537b37e1bb587f2c0f0f0
kayatsa <kayatsa8@gmail.com>
2024-11-18 17:22:34 +0200

    Backend/Board: updatePosition completed.

--- Backend/Board.java
+++ Backend/Board.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import Backend.Tiles.EmptyTile;
 import Backend.Tiles.Tile;
 import Backend.Tiles.Unit.Unit;
 
@@ -61,8 +62,15 @@ public class Board {
         return board[y][x];
     }
 
-    public void updatePositions(Unit u1, Tile tile){
-        throw new RuntimeException("not implemented yet");
+    public void updatePositions(Unit u, Tile tile){
+        board[u.getPosition().getY()][u.getPosition().getX()] = u;
+
+        // a dead enemy
+        if(tile.getTile() == '1'){
+            tile = new EmptyTile('.', tile.getPosition().getX(), tile.getPosition().getY());
+        }
+
+        board[tile.getPosition().getY()][tile.getPosition().getX()] = tile;
     }
 
 }
